160ec3ef91775f847776984fb37b71a3ed6fddee
Alex Buly <asbuly@icloud.com>
2025-07-29 14:25:32 -0400

    Edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Header } from "./components/Header"
-import { Education } from "./components/Education"
-import { Work } from "./components/Work"
+import GeneralInfo from "./components/GeneralInfo";
+import { Education } from "./components/Education";
 import "./styles/style.css";
+import { Experience } from "./components/Experience";
 
 function App() {
   return (
     <>
-      <Header />
-      <Education />
-      <Work />
+    <div className="container">
+      <h1>CV Application</h1>
+       <GeneralInfo/>
+       <Education/>
+       <Experience/>
+    </div>
     </>
   )
 }
